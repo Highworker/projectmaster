6e3350f57e789ba6f7557adcfd8f149ebd631a3c
Highworker <derkampfer@yandex.ru>
2022-03-13 11:33:20 +0300

    Add stuff for Drinks

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ControllerDrinks;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::view('/', 'default', [ControllerDrinks::class, 'show', 'title' => 'Drinks']);
+Route::get('/', [ControllerDrinks::class, 'index']);
+
