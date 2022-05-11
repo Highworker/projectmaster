b616c05c0c5dcbed1b7ee9d0d12b3fff33ef2c57
Highworker <derkampfer@yandex.ru>
2022-05-11 18:47:49 +0300

    +some "unversioned" files, cant understand why migration and request not added by def

---
+++ database/migrations/2022_04_21_135626_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\User;
+use App\Models\Drink;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Drink::class);
+            $table->timestamps();
+            $table->string('text');
+            $table->integer('rating')->nullable(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
