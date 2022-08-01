83971074376eb95707175f68a30c99628352b222
Highworker <derkampfer@yandex.ru>
2022-08-01 18:20:14 +0300

    fixig login to orhid - 1

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+require __DIR__.'/auth.php';
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
 Route::get('/', [DrinkController::class, 'index']);
 Route::get('/drink/{id}', [DrinkController::class, 'show']);
 Route::get('/ingridients', [IngridientController::class, 'index']);
