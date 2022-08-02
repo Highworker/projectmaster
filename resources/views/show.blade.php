8789c5247ceb125ab6cf1b816d778b2875fdd5ae
Highworker <derkampfer@yandex.ru>
2022-08-02 17:01:59 +0300

    markdown +

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{$drink->name}}</title>
     <link rel="stylesheet" href="/css/styles.css">
+    <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+    <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </head>
 <body>
 @include('header')
